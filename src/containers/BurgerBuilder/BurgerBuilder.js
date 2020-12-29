758539dc5b0d5641e4d80a4ba94b294aef26548a
NanWu <nancypty@gmail.com>
2020-12-29 09:16:34 -0800

    Send a POST request to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,7 +3,8 @@ import Aux from '../../hoc/Auxiliary/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -72,7 +73,26 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        // send request to backend
+        const order = {
+            ingredients: this.state.ingredients,
+            //in real project, this would be calculated on the server (backend)
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Nan Wu',
+                address: {
+                    street: 'TestStreet 1',
+                    zipcode: '98888',
+                    country: 'US'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'ups'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render () {
