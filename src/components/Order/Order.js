b7c5ea1346012635e76c2aa15fc21861f52523de
NanWu <nancypty@gmail.com>
2020-12-31 21:33:17 -0800

    Fetching orders from backend and output orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad (1)</p>
-        <p>Price: <string>USD 5.45</string></p>
-    </div>
-);
+const order = (props) => {
+    // transform ingredients
+    // we have same logic in Burger.js, here we use another way to do it
+    const ingredients = [];
+    
+    for (let ingredientName in props.ingredients) {
+        ingredients.push(
+            {
+                name: ingredientName, 
+                amount: props.ingredients[ingredientName],
+            })
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span
+                style={{
+                    textTransform: 'capitalize', 
+                    display: 'inline-block',
+                    margin: '0 8px',
+                    border: '1px solid #ccc',
+                    padding: '5px'
+                }}
+                key={ig.name} >{ig.name} ({ig.amount})</span>;
+    })
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <string>USD {Number.parseFloat(props.price).toFixed(2)}</string></p>
+        </div>
+    );
+    
+};
 
 export default order;
