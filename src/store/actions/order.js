b9042c83377171f55b8e1f6f7d089384ffc70587
NanWu <nancypty@gmail.com>
2021-01-17 20:35:16 -0800

    Fetching Orders via redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,45 +1,89 @@
-import * as actionTypes from './actionTypes';
-import axios from '../../axios-orders';
-
+import * as actionTypes from "./actionTypes";
+import axios from "../../axios-orders";
 
 export const purchaseBurgerSuccess = (id, orderData) => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_SUCCESS,
-        orderId: id,
-        orderData: orderData
-    };
+  return {
+    type: actionTypes.PURCHASE_BURGER_SUCCESS,
+    orderId: id,
+    orderData: orderData,
+  };
 };
 
 export const purchaseBurgerFail = (error) => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_FAIL,
-        error: error
-    };
+  return {
+    type: actionTypes.PURCHASE_BURGER_FAIL,
+    error: error,
+  };
 };
 
 export const purchaseBurgerStart = () => {
-    return {
-        type: actionTypes.PURCHASE_BURGER_START
-    };
+  return {
+    type: actionTypes.PURCHASE_BURGER_START,
+  };
 };
 
 // async action we dispatched from the container once we clicke the order button
 export const purchaseBurger = (orderData) => {
-    return dispatch => {
-        dispatch(purchaseBurgerStart());
-        axios.post('/orders.json', orderData)
-            .then(response => {
-                console.log(response.data);
-                dispatch(purchaseBurgerSuccess(response.data.name, orderData));
-            })
-            .catch(error => {
-                dispatch(purchaseBurgerFail(error));
-            } );
-    };
+  return (dispatch) => {
+    dispatch(purchaseBurgerStart());
+    axios
+      .post("/orders.json", orderData)
+      .then((response) => {
+        console.log(response.data);
+        dispatch(purchaseBurgerSuccess(response.data.name, orderData));
+      })
+      .catch((error) => {
+        dispatch(purchaseBurgerFail(error));
+      });
+  };
 };
 
 export const purchaseInit = () => {
-    return {
-        type: actionTypes.PURCHASE_INIT
-    };
+  return {
+    type: actionTypes.PURCHASE_INIT,
+  };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDER_SUCCESS,
+    orders: orders,
+  };
+};
+
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDER_FAIL,
+    error: error,
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDER_START,
+  };
+};
+
+// async
+export const fetchOrders = () => {
+  return (dispatch) => {
+    dispatch(fetchOrdersStart());
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        console.log(res.data);
+        // turn orders object into an array
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch((err) => {
+        dispatch(fetchOrdersFail(err));
+      });
+  };
 };
